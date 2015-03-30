3bc77742d66531aba339ced21403182e79d54112
Andrew <andrewdude28@gmail.com>
2015-03-30 15:14:14 -0700

    flighplan fixed

--- flightplan.js
+++ flightplan.js
@@ -32,11 +32,11 @@ plan.target('production', [
 // run commands on localhost
 plan.local(function(local) {
   // uncomment these if you need to run a build on your machine first
-  // local.log('Run build');
-  // local.exec('gulp build');
+  local.log('Run build');
+  local.exec('gulp build');
 
   local.log('Copy files to remote hosts');
-  var filesToCopy = local.exec('git ls-files', {silent: true});
+  var filesToCopy = local.exec('git ls-files; find build -type f', {silent: true});
   // rsync files to all the destination's hosts
   local.transfer(filesToCopy, '/tmp/' + tmpDir);
 });
